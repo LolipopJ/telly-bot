2005831153763341383b34e72265fbe9e2e07bc4
JasonSun <lolipop0703@qq.com>
2022-08-20 18:39:35 +0800

    fix: fix typo

--- src/assets/index.js
+++ src/assets/index.js
@@ -109,7 +109,9 @@ const resolvePixivDataObject = (pixivDataObject) => {
 
     data.picName = `${picId}_p${picIndex}.${picType}`
     data.picNameMD = `${picId}\\_p${picIndex}\\.${picType}`
-    data.picSizeMd = (data.picSize ?? '0.0').spilt('.').join('\\.')
+    data.picSizeMd = String(data.picSize ?? '0.0')
+        .split('.')
+        .join('\\.')
     data.picUrl = `https://www.pixiv.net/artworks/${picId}`
 
     let picProxyUrlParam
